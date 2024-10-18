c1cff6125e9a30cc871a4c8daa095fadc82b12f8
Vladimir Bolshakov <vladalexeco86@gmail.com>
2024-10-18 14:20:24 +0300

    created clean architecture frame

--- app/src/main/java/ru/vladalexeco/lazyprogrammer/presentation/activities/MainActivity.kt
+++ app/src/main/java/ru/vladalexeco/lazyprogrammer/presentation/activities/MainActivity.kt
@@ -11,11 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 import ru.vladalexeco.lazyprogrammer.core.navigation.Alarms
 import ru.vladalexeco.lazyprogrammer.core.navigation.AppNavScreen
 import ru.vladalexeco.lazyprogrammer.presentation.ui.screens.CreateTaskScreen
 import ru.vladalexeco.lazyprogrammer.presentation.ui.theme.LazyProgrammerTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
